24aca4eb4dc9e9fed6b984d368283c9c3e12d193
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-07-18 06:20:58 -0700

    first commit

---
+++ _test_/triangle.test.js
@@ -0,0 +1,8 @@
+import Triangle from '../src/js/triangle.';
+
+describe('Triangle', () => {
+
+    Test(should correctly creat a triangle object with three lengths' , () => {
+      const triangle = new Triangle(2,4,5);
+      expect(triangle.side1).toEqual(2);
+      
