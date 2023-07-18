1670dce494b6e9b6fabf8e6660f0c4976a1dca0b
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-07-18 06:28:55 -0700

    added first commmit

--- _test_/triangle.test.js
+++ _test_/triangle.test.js
@@ -5,4 +5,4 @@ describe('Triangle', () => {
     Test(should correctly creat a triangle object with three lengths' , () => {
       const triangle = new Triangle(2,4,5);
       expect(triangle.side1).toEqual(2);
-      
+
